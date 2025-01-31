88a25ce8f0e61c1fc92371c8812bdbd750103e87
egagli <egagli@uw.edu>
2025-01-30 17:44:49 -0800

    revert changes, didnt fix shapely issue

--- tests/test_hydroclimatology.py
+++ tests/test_hydroclimatology.py
@@ -22,21 +22,21 @@
 
 
 # https://github.com/gee-community/ee-initialize-github-actions?tab=readme-ov-file#4-initialize-to-earth-engine-in-your-test-file
-import ee
-import json
-import os
-import google.oauth2.credentials
+# import ee
+# import json
+# import os
+# import google.oauth2.credentials
 
-stored = json.loads(os.getenv("EARTHENGINE_TOKEN"))
-credentials = google.oauth2.credentials.Credentials(
-    None,
-    token_uri="https://oauth2.googleapis.com/token",
-    client_id=stored["client_id"],
-    client_secret=stored["client_secret"],
-    refresh_token=stored["refresh_token"],
-    quota_project_id=stored["project"],
-)
+# stored = json.loads(os.getenv("EARTHENGINE_TOKEN"))
+# credentials = google.oauth2.credentials.Credentials(
+#     None,
+#     token_uri="https://oauth2.googleapis.com/token",
+#     client_id=stored["client_id"],
+#     client_secret=stored["client_secret"],
+#     refresh_token=stored["refresh_token"],
+#     quota_project_id=stored["project"],
+# )
 
-ee.Initialize(credentials=credentials)
+# ee.Initialize(credentials=credentials)
 
-print(ee.String("Greetings from the Earth Engine servers!").getInfo())
+# print(ee.String("Greetings from the Earth Engine servers!").getInfo())
